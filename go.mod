50631a4db0733e8b76ca327d1001e793bffe0d07
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-09-15 09:08:56 +0300

    chores: download sqlx and pq libraries and created connection with db

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/advor2102/socialnetwork
 
 go 1.24.5
+
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	dns := "host=localhost port=5432 user=postgres password=!Makar24052018 dname=social_network_db sslmode=disable"
+
+	db, err := sqlx.Open("postgres", dns)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
 }
