d49dffcb24c217bacac9c0239f990bd1cb75a5c1
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-09-15 09:18:23 +0300

    chores: created repository structure and draft of the function

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,4 +1,13 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Repository struct {
+	db *sqlx.DB
 }
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{db: db}
+}
+
+func (r *Repository) GetAllUsers()
