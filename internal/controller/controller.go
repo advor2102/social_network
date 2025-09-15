e4ec1795937d82bb345e61ff3b1862f790433901
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-09-15 10:26:57 +0300

    chores: created new structures for controller and services and basic function

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,4 +1,11 @@
 package controller
 
+import "github.com/advor2102/socialnetwork/internal/service"
+
 type Controller struct {
+	service *service.Service
+}
+
+func NewController(service *service.Service) *Controller {
+	return &Controller{service: service}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,4 +1,11 @@
 package service
 
+import "github.com/advor2102/socialnetwork/internal/repository"
+
 type Service struct {
+	repository *repository.Repository
+}
+
+func NewService(repository *repository.Repository) *Service {
+	return &Service{repository: repository}
 }
