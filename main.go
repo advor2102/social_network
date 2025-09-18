4cb2b67faf7b4e44a43d6f6912f440c5cd004dab
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-09-18 09:37:26 +0300

    feat: covered sensitive data under configs in main

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/advor2102/socialnetwork/internal/configs"
 	"github.com/advor2102/socialnetwork/internal/controller"
 	"github.com/advor2102/socialnetwork/internal/repository"
 	"github.com/advor2102/socialnetwork/internal/service"
@@ -11,7 +13,22 @@ import (
 )
 
 func main() {
-	dns := "host=localhost port=5432 user=postgres password=!Makar24052018 dbname=social_network_db sslmode=disable"
+	if err := configs.ReadSettings(); err != nil {
+		log.Fatal(err)
+	}
+
+	dns := fmt.Sprintf(`host=%s 
+						port=%s
+						user=%s 
+						password=%s 
+						dbname=%s 
+						sslmode=disable`,
+		configs.AppSettings.PostgresParams.Host,
+		configs.AppSettings.PostgresParams.Port,
+		configs.AppSettings.PostgresParams.User,
+		configs.AppSettings.PostgresParams.Password,
+		configs.AppSettings.PostgresParams.Database,
+	)
 
 	db, err := sqlx.Open("postgres", dns)
 	if err != nil {
@@ -22,7 +39,7 @@ func main() {
 	service := service.NewService(repository)
 	controller := controller.NewController(service)
 
-	if err = controller.RunServer(":7777"); err != nil {
+	if err = controller.RunServer(fmt.Sprintf(":%s", configs.AppSettings.AppParams.PortRun)); err != nil {
 		log.Fatal(err)
 	}
 
