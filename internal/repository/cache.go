074fd1bb8c60165888bc87644f6b8990b62ccd15
Alex Dvornikov <a.dvornikov2102@gmail.com>
2025-09-19 19:48:41 +0300

    feat: added universal function for using redis

--- internal/repository/cache.go
+++ internal/repository/cache.go
@@ -1,6 +1,13 @@
 package repository
 
-import "github.com/redis/go-redis/v9"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
 
 type Cache struct {
 	rdb *redis.Client
@@ -10,4 +17,34 @@ func NewCache(client *redis.Client) *Cache {
 	return &Cache{
 		rdb: client,
 	}
+}
+
+func (c *Cache) Set(rdb *redis.Client, ctx context.Context, key string, value interface{}, duration time.Duration) error {
+	rawU, err := json.Marshal(value)
+	if err != nil {
+		fmt.Println("error during marshal:", err)
+		return err
+	}
+
+	if err = c.rdb.Set(ctx, key, rawU, duration).Err(); err != nil {
+		fmt.Println("error during set:", err)
+		return err
+	}
+
+	return nil
+}
+
+func (c *Cache) Get(ctx context.Context, key string, response interface{}) error {
+	val, err := c.rdb.Get(ctx, key).Result()
+	if err != nil {
+		fmt.Println("error during get:", err)
+		return err
+	}
+
+	if err = json.Unmarshal([]byte(val), response); err != nil {
+		fmt.Println("error during unmarshal", err)
+		return err
+	}
+
+	return nil
 }
